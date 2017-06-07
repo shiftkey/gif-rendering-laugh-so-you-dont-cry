ae76e5185f5abbebf1d20049b58046cc1aa46637
Brendan Forster <brendan@github.com>
2017-06-07 14:29:29 -0300

    tidy up repro

--- WpfApp1/Program.cs
+++ WpfApp1/Program.cs
@@ -1,35 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    static class Program
     {
-        protected override void OnStartup(StartupEventArgs e)
+        static void Main()
         {
-            base.OnStartup(e);
-
             var animatedGifWindowToken = new CancellationTokenSource();
 
             var progressSource = new ProgressSource();
-            
+
             AnimatedGifWindow.ShowWindow(TimeSpan.FromSeconds(1), animatedGifWindowToken.Token, progressSource);
 
             Task.Delay(TimeSpan.FromMinutes(5)).Wait();
-            
+
             animatedGifWindowToken.Cancel();
         }
     }
-
 
     public class ProgressSource
     {
@@ -41,6 +30,5 @@ namespace WpfApp1
                 Progress.Invoke(this, i);
         }
     }
+
 }
-
-
